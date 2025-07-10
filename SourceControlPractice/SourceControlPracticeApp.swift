470c337f320afaf41fe611edb34262ff32f5073c
joe <joescholtz@hotmail.com>
2025-07-10 09:35:27 -0500

    Initial Commit

---
+++ SourceControlPractice/SourceControlPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlPracticeApp.swift
+//  SourceControlPractice
+//
+//  Created by Joseph Scholtz on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
